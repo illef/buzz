e7d2e6e1059426aafa68cfddb66f41d685a7e132
taylor <taylor@jennifersoft.com>
2020-06-19 16:18:20 +0900

    Run systray::Application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let mut app = match systray::Application::new() {
+        Ok(app) => app,
+        Err(e) => {
+            println!("Could not create gtk application: {}", e);
+            return;
+        }
+    };
+
+    if let Err(e) = app
+        .set_icon_from_file(&"/usr/share/icons/oxygen/base/32x32/status/mail-read.png".to_string())
+    {
+        println!("Could not set application icon: {}", e);
+    }
+
+    app.wait_for_message().unwrap();
 }
